a733fa6085c0321f5576178965a308bcceabb75b
Xin.Yuan <xin.yuan@partner.bmw.com>
2022-02-16 18:08:32 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int array[] ={0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        Solution sol = new Solution();
+        sol.removeDuplicates(array);
+    }
+}
